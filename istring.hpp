d4132693dfd271240ec4d3ba7f5e4f6c187b5896
hewill <67143766+hewillk@users.noreply.github.com>
2021-03-24 16:55:53 +0800

    Update istring.hpp

--- istring.hpp
+++ istring.hpp
@@ -9,19 +9,18 @@
 namespace biomodern::utility {
 
 using namespace std::string_literals;
-using ichar = std::int8_t;
-using istring = std::basic_string<ichar>;
-using istring_view = std::basic_string_view<ichar>;
+using istring = std::basic_string<std::int8_t>;
+using istring_view = std::basic_string_view<std::int8_t>;
 
-inline auto operator""_is(const char* s, std::size_t size) {
+inline auto operator""_s(const char* s) {
   auto is = istring{};
-  for (auto i = 0; i < size; i++) is.push_back(s[i] - '0');
+  for (const auto c : std::string_view{s}) is.push_back(c - '0');
   return is;
 }
 
 struct Codec {
   constexpr static auto ints = [] {
-    auto ints = std::array<ichar, 128>{};
+    auto ints = std::array<std::int8_t, 128>{};
     ints.fill(4);
     ints['a'] = ints['A'] = 0;
     ints['c'] = ints['C'] = 1;
@@ -41,7 +40,7 @@ struct Codec {
   };
 
   static auto rhash(std::size_t key, std::size_t size) {
-    auto seq = ""_is;
+    auto seq = istring{};
     for (auto i = 0; i < size; i++) {
       const auto shift = (size - i - 1) * 2;
       seq += (key & 3ull << shift) >> shift;
@@ -50,7 +49,7 @@ struct Codec {
   }
 
   static auto rev_comp(istring_view seq) {
-    auto res = ""_is;
+    auto res = istring{};
     res.reserve(seq.size());
     std::transform(seq.rbegin(), seq.rend(), std::back_inserter(res), [](const auto c) {
       return c == 4 ? 4 : 3 - c;
@@ -59,7 +58,7 @@ struct Codec {
   }
 
   static auto to_istring(std::string_view seq) {
-    auto res = ""_is;
+    auto res = istring{};
     res.reserve(seq.size());
     std::transform(seq.begin(), seq.end(), std::back_inserter(res), to_int);
     return res;
@@ -80,7 +79,7 @@ struct Codec {
   };
 
   static auto rev_comp(std::string_view seq) {
-    auto res = ""s;
+    auto res = std::string{};
     res.reserve(seq.size());
     std::transform(seq.rbegin(), seq.rend(), std::back_inserter(res), comp);
     return res;
